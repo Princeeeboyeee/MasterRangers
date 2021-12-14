e889124b9f58ea69795eaa4640b55877744e2525
Princeeeboyeee <88536083+Princeeeboyeee@users.noreply.github.com>
2021-12-14 15:14:03 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+
+var canvas=new fabric.Canvas('canvas');
+block_width=20;
+block_height=20;
+player_x=10;
+player_y=20;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image){
+    fabric.Image.fromURL(get_image, function (Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_width);
+        block_image_object.scaleToHeight(block_height);
+        block_image_object.set({
+         top:player_y,left:player_x   
+        });
+        canvas.add(block_image_object);
+    });
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		new_image('rr.jpg');
+		block_x=100;
+	}
+	if(keyPressed == '71')
+	{
+		new_image('gr.png');
+		block_x = 200;
+		// upload green ranger
+	}
+	
+	if(keyPressed == '89')
+	{
+		new_image('yr.png');
+		block_x =350;
+		
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		new_image('pr.png');
+	}
+	if(keyPressed == '66')
+	{
+		new_image('br.png');
+		block_x = 700;
+	
+	}}}
